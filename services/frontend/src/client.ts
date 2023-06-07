7af503593e1f918d40819afc5cc91f2271bf0164
gk121 <gunnar.kistner@gmail.com>
2023-06-07 23:27:17 +0200

    added subsscription plugin

--- services/frontend/src/client.ts
+++ services/frontend/src/client.ts
@@ -1,7 +1,13 @@
-import { HoudiniClient } from '$houdini';
+import { createClient } from 'graphql-ws'
+import { HoudiniClient, subscription } from '$houdini';
 
 export default new HoudiniClient({
-    url: "http://0.0.0.0:5001/graphql"
+    url: "http://0.0.0.0:5001/graphql",
+    plugins: [
+        subscription(() => createClient({
+          url: 'ws://localhost:5000/graphql'
+        }))
+      ]
 
     // uncomment this to configure the network call (for things like authentication)
     // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
